2dceb4c24238b03bfc44e3fcd3e288b77d99e4f9
Alina O <alinapiatakova1309@gmail.com>
2024-10-09 23:35:37 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var myNum = 10;
+var myStr = 'some string';
+var myBool = true;
+var myArr = [1, 2, 3, 4, 5];
+var myObj = {
+    first: 'First Name',
+     last: 'Last Name'
+}
+console.log(myObj) 
+
+var myNum = 10;
+var decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+
+var myBigInt= 123n;
+myBigInt += 1n;
+console.log(myBigInt)
